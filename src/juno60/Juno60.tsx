4415671606117a304efc900cf419d7c8fab29484
Ethan223 <ethan.coles223@gmail.com>
2020-02-15 23:28:26 -0500

    Put thead and tbody in tables

--- src/juno60/Juno60.tsx
+++ src/juno60/Juno60.tsx
@@ -4,55 +4,63 @@ import './Juno60.css'
 class Juno60 extends Component {
     render() {
         return(
-            <div className="App">
-                <h1>JUNO-60 Editor</h1>
-                <table className="main-board">
-                    <tr>
-                        <th>LFO</th>
-                        <th>DCO</th>
-                        <th>HPF</th>
-                    </tr>
-                    <tr>
-                        {/*LFO contents*/}
-                        <td className="main-board">
-                            {this.renderLFO()}
-                        </td>
-                        {/* DCO contents */}
-                        <td className="main-board">
-                            {this.renderDCO()}
-                        </td>
-                        {/* HPF contents */}
-                        <td className="main-board">
-                            {this.renderHPF()}            
-                        </td>        
-                    </tr>
+            <div className="main">
+                <h1>Roland JUNO-60 Editor</h1>
+                <table className="board-comp">
+                    <thead>
+                        <tr>
+                            <th>LFO</th>
+                            <th>DCO</th>
+                            <th>HPF</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            {/*LFO contents*/}
+                            <td className="board-comp">
+                                {this.renderLFO()}
+                            </td>
+                            {/* DCO contents */}
+                            <td className="board-comp">
+                                {this.renderDCO()}
+                            </td>
+                            {/* HPF contents */}
+                            <td className="board-comp">
+                                {this.renderHPF()}            
+                            </td>        
+                        </tr>
+                    </tbody>
                 </table>
                 <br/>
-                <table className="main-board">
-                    <tr>
-                        <th>VCF</th>
-                        <th>VCA</th>
-                        <th>ENV</th>
-                        <th>CHORUS</th>
-                    </tr>
-                    <tr>
-                        {/* VCF contents */}
-                        <td className="main-board">
-                            {this.renderVCF()}
-                        </td>
-                        {/* VCA contents */}
-                        <td className="main-board">
-                            {this.renderVCA()} 
-                        </td>
-                        {/* ENV contents */}
-                        <td className="main-board">
-                            {this.renderENV()}  
-                        </td>
-                        {/* CHORUS contents */}
-                        <td className="chorus">
-                            {this.renderCHORUS()} 
-                        </td>
-                    </tr> 
+                <table className="board-comp">
+                    <thead>
+                        <tr>
+                            <th>VCF</th>
+                            <th>VCA</th>
+                            <th>ENV</th>
+                            <th>CHORUS</th>
+                        </tr>
+                    </thead>                    
+                    <tbody>
+                        <tr>
+                            {/* VCF contents */}
+                            <td className="board-comp">
+                                {this.renderVCF()}
+                            </td>
+                            {/* VCA contents */}
+                            <td className="board-comp">
+                                {this.renderVCA()} 
+                            </td>
+                            {/* ENV contents */}
+                            <td className="board-comp">
+                                {this.renderENV()}  
+                            </td>
+                            {/* CHORUS contents */}
+                            <td className="chorus">
+                                {this.renderCHORUS()} 
+                            </td>
+                        </tr>
+                    </tbody>                     
                 </table>
             </div>
         );
@@ -61,23 +69,27 @@ class Juno60 extends Component {
     renderLFO(): JSX.Element {
         return(
             <table>
-                <tr>
-                    <td>RATE</td>
-                    <td>DELAY TIME</td>
-                    <td>TRIG MODE</td>
-                </tr>
-                <tr>
-                    <td>
-                        <input type="range" min="0" max="10" step="0.5" defaultValue="0"></input>
-                    </td>
-                    <td>
-                        <input type="range" min="0" max="10" step="0.5" defaultValue="0"></input>
-                    </td>
-                    <td className="radioButton">
-                        <input type="radio" name="trigMode" value="auto" checked></input>AUTO<br></br>
-                        <input type="radio" name="trigMode" value="man"></input>MAN<br></br>
-                    </td>
-                </tr> 
+                <thead>
+                    <tr>
+                        <td>RATE</td>
+                        <td>DELAY TIME</td>
+                        <td>TRIG MODE</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <input type="range" min="0" max="10" step="0.5" defaultValue="0"></input>
+                        </td>
+                        <td>
+                            <input type="range" min="0" max="10" step="0.5" defaultValue="0"></input>
+                        </td>
+                        <td className="radioButton">
+                            <input type="radio" name="trigMode" value="auto" checked></input>AUTO<br></br>
+                            <input type="radio" name="trigMode" value="man"></input>MAN<br></br>
+                        </td>
+                    </tr> 
+                </tbody>
             </table>
         )
     }
@@ -85,38 +97,42 @@ class Juno60 extends Component {
     renderDCO(): JSX.Element {
         return(
             <table>
-                <tr>
-                    <td>LFO</td>
-                    <td>PWM</td>
-                    <td></td>
-                    <td>SQU&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;SAW&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;SUB</td>
-                    <td>SUB OSC</td>
-                    <td>NOISE</td>
-                </tr>
-                <tr>
-                    <td>
-                        <input type="range" min="0" max="10" step="0.5" defaultValue="0"></input>
-                    </td>
-                    <td>
-                        <input type="range" min="0" max="10" step="0.5" defaultValue="0"></input>
-                    </td>
-                    <td className="radioButton">
-                        <input type="radio" name="pulseWidthMod" value="lfo"></input>LFO<br></br>
-                        <input type="radio" name="pulseWidthMod" value="manual" checked></input>MAN<br></br>
-                        <input type="radio" name="pulseWidthMod" value="env"></input>ENV<br></br>
-                    </td>
-                    <td className="chorus">
-                        <input type="checkbox"></input>
-                        <input type="checkbox"></input>
-                        <input type="checkbox"></input>
-                    </td>
-                    <td>
-                        <input type="range" min="0" max="10" step="0.5" defaultValue="0"></input>
-                    </td>
-                    <td>
-                        <input type="range" min="0" max="10" step="0.5" defaultValue="0"></input>
-                    </td>
-                </tr>
+                <thead>
+                    <tr>
+                        <td>LFO</td>
+                        <td>PWM</td>
+                        <td></td>
+                        <td>SQU&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;SAW&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;SUB</td>
+                        <td>SUB OSC</td>
+                        <td>NOISE</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <input type="range" min="0" max="10" step="0.5" defaultValue="0"></input>
+                        </td>
+                        <td>
+                            <input type="range" min="0" max="10" step="0.5" defaultValue="0"></input>
+                        </td>
+                        <td className="radioButton">
+                            <input type="radio" name="pulseWidthMod" value="lfo"></input>LFO<br></br>
+                            <input type="radio" name="pulseWidthMod" value="manual" checked></input>MAN<br></br>
+                            <input type="radio" name="pulseWidthMod" value="env"></input>ENV<br></br>
+                        </td>
+                        <td className="chorus">
+                            <input type="checkbox"></input>
+                            <input type="checkbox"></input>
+                            <input type="checkbox"></input>
+                        </td>
+                        <td>
+                            <input type="range" min="0" max="10" step="0.5" defaultValue="0"></input>
+                        </td>
+                        <td>
+                            <input type="range" min="0" max="10" step="0.5" defaultValue="0"></input>
+                        </td>
+                    </tr>
+                </tbody>
             </table>
         )
     }
@@ -124,14 +140,18 @@ class Juno60 extends Component {
     renderHPF(): JSX.Element {
         return(
             <table>
-                <tr>
-                    <td>FREQ</td> 
-                </tr>
-                <tr>
-                    <td>
-                        <input type="range" min="0" max="3" step="1" defaultValue="0"></input>
-                    </td>
-                </tr>
+                <thead>
+                    <tr>
+                        <td>FREQ</td> 
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <input type="range" min="0" max="3" step="1" defaultValue="0"></input>
+                        </td>
+                    </tr>
+                </tbody>
             </table>
         )
     }
@@ -139,35 +159,39 @@ class Juno60 extends Component {
     renderVCF(): JSX.Element {
         return(
             <table>
-                <tr>
-                    <td>FREQ</td>
-                    <td>RES</td>
-                    <td>-</td>
-                    <td>ENV</td>
-                    <td>LFO</td>
-                    <td>KYBD</td>
-                </tr>
-                <tr>
-                    <td>
-                        <input type="range" min="0" max="10" step="0.5" defaultValue="10"></input>
-                    </td>
-                    <td>
-                        <input type="range" min="0" max="10" step="0.5" defaultValue="0"></input>
-                    </td>
-                    <td className="radioButton">
-                        <input type="radio" name="envPolarity" value="pos" checked></input>+<br></br>
-                        <input type="radio" name="envPolarity" value="neg"></input>-<br></br>
-                    </td>
-                    <td>
-                        <input type="range" min="0" max="10" step="0.5" defaultValue="0"></input>
-                    </td>
-                    <td>
-                        <input type="range" min="0" max="10" step="0.5" defaultValue="0"></input>
-                    </td>
-                    <td>
-                        <input type="range" min="0" max="10" step="0.5" defaultValue="0"></input>
-                    </td>
-                </tr>
+                <thead>
+                    <tr>
+                        <td>FREQ</td>
+                        <td>RES</td>
+                        <td>-</td>
+                        <td>ENV</td>
+                        <td>LFO</td>
+                        <td>KYBD</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <input type="range" min="0" max="10" step="0.5" defaultValue="10"></input>
+                        </td>
+                        <td>
+                            <input type="range" min="0" max="10" step="0.5" defaultValue="0"></input>
+                        </td>
+                        <td className="radioButton">
+                            <input type="radio" name="envPolarity" value="pos" checked></input>+<br></br>
+                            <input type="radio" name="envPolarity" value="neg"></input>-<br></br>
+                        </td>
+                        <td>
+                            <input type="range" min="0" max="10" step="0.5" defaultValue="0"></input>
+                        </td>
+                        <td>
+                            <input type="range" min="0" max="10" step="0.5" defaultValue="0"></input>
+                        </td>
+                        <td>
+                            <input type="range" min="0" max="10" step="0.5" defaultValue="0"></input>
+                        </td>
+                    </tr>
+                </tbody>
             </table>
         ) 
     }
@@ -175,19 +199,23 @@ class Juno60 extends Component {
     renderVCA(): JSX.Element {
         return(
             <table>
-                <tr>
-                    <td>-</td>
-                    <td>LEVEL</td>
-                </tr>
-                <tr>
-                    <td className="radioButton">
-                        <input type="radio" name="vca" value="env"></input>ENV<br></br>
-                        <input type="radio" name="vca" value="gate" checked></input>GATE<br></br>
-                    </td>
-                    <td>
-                        <input type="range" min="0" max="10" step="0.5" defaultValue="5"></input>
-                    </td>
-                </tr>
+                <thead>
+                    <tr>
+                        <td>-</td>
+                        <td>LEVEL</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td className="radioButton">
+                            <input type="radio" name="vca" value="env"></input>ENV<br></br>
+                            <input type="radio" name="vca" value="gate" checked></input>GATE<br></br>
+                        </td>
+                        <td>
+                            <input type="range" min="0" max="10" step="0.5" defaultValue="5"></input>
+                        </td>
+                    </tr>
+                </tbody>
             </table>
         )
     }
@@ -195,26 +223,30 @@ class Juno60 extends Component {
     renderENV(): JSX.Element {
         return(
             <table>
-                <tr>
-                    <td>A</td>
-                    <td>D</td>
-                    <td>S</td>
-                    <td>R</td>
-                </tr>
-                <tr>
-                    <td>
-                        <input type="range" min="0" max="10" step="0.5" defaultValue="0"></input>
-                    </td>
-                    <td>
-                        <input type="range" min="0" max="10" step="0.5" defaultValue="0"></input>
-                    </td>
-                    <td>
-                        <input type="range" min="0" max="10" step="0.5" defaultValue="10"></input>
-                    </td>
-                    <td>
-                        <input type="range" min="0" max="10" step="0.5" defaultValue="0"></input>
-                    </td>  
-                </tr> 
+                <thead>
+                    <tr>
+                        <td>A</td>
+                        <td>D</td>
+                        <td>S</td>
+                        <td>R</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <input type="range" min="0" max="10" step="0.5" defaultValue="0"></input>
+                        </td>
+                        <td>
+                            <input type="range" min="0" max="10" step="0.5" defaultValue="0"></input>
+                        </td>
+                        <td>
+                            <input type="range" min="0" max="10" step="0.5" defaultValue="10"></input>
+                        </td>
+                        <td>
+                            <input type="range" min="0" max="10" step="0.5" defaultValue="0"></input>
+                        </td>  
+                    </tr> 
+                </tbody>                
             </table>
         )
     }
@@ -222,22 +254,26 @@ class Juno60 extends Component {
     renderCHORUS(): JSX.Element {
         return(
             <table>
-                <tr>
-                    <td>OFF</td>
-                    <td>I</td>
-                    <td>II</td>
-                </tr>
-                <tr>
-                    <td>
-                        <input type="checkbox" checked></input>
-                    </td>
-                    <td>
-                        <input type="checkbox"></input>
-                    </td>
-                    <td>
-                        <input type="checkbox"></input>
-                    </td>
-                </tr>
+                <thead>
+                    <tr>
+                        <td>OFF</td>
+                        <td>I</td>
+                        <td>II</td>
+                    </tr>
+                </thead>
+                <tbody>                
+                    <tr>
+                        <td>
+                            <input type="checkbox" checked></input>
+                        </td>
+                        <td>
+                            <input type="checkbox"></input>
+                        </td>
+                        <td>
+                            <input type="checkbox"></input>
+                        </td>
+                    </tr>
+                </tbody>
             </table>
         )
     }
