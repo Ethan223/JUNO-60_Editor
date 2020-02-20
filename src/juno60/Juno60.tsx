b065c32b4e78d4d33e087d644a3966bc23e4b241
Ethan223 <ethan.coles223@gmail.com>
2020-02-19 21:38:59 -0500

    Figure out states with radio buttons, but don't know why...

--- src/juno60/Juno60.tsx
+++ src/juno60/Juno60.tsx
@@ -85,7 +85,10 @@ interface LFOProps {
 
 const LFO: React.FC<LFOProps> = props => {
     const [state, setState] = useState<LFOState>({LFO_RATE: 0, LFO_DELAY: 0, TRIG_MODE: LfoTriggerMode.AUTO});
-    const updateStateHandler = (key: keyof LFOState) => (ev: React.ChangeEvent<HTMLInputElement>) => setState({ ...state, [key]: Number(ev.target.value)});
+    const updateStateHandler = (key: keyof LFOState) => (ev: React.ChangeEvent<HTMLInputElement>) => {
+        setState({ ...state, [key]: Number(ev.target.value)});
+        console.log(ev.target.value);
+    }
     props.onChange(state);
 
     return(
@@ -100,14 +103,14 @@ const LFO: React.FC<LFOProps> = props => {
             <tbody>
                 <tr>
                     <td>
-                        <input type="range" min="0" max="10" step="0.5" defaultValue={state.LFO_RATE} onChange={updateStateHandler("LFO_RATE")}></input>
+                        <input type="range" min="0" max="10" step="0.5" value={state.LFO_RATE} onChange={updateStateHandler("LFO_RATE")}></input>
                     </td>
                     <td>
-                        <input type="range" min="0" max="10" step="0.5" defaultValue={state.LFO_DELAY} onChange={updateStateHandler("LFO_DELAY")}></input>
+                        <input type="range" min="0" max="10" step="0.5" value={state.LFO_DELAY} onChange={updateStateHandler("LFO_DELAY")}></input>
                     </td>
                     <td className="radioButton">
-                        <input type="radio" name="trigMode" value="auto" checked></input>AUTO<br></br>
-                        <input type="radio" name="trigMode" value="man"></input>MAN<br></br>
+                        <input type="radio" name="trigMode" value={LfoTriggerMode.AUTO} checked={state.TRIG_MODE === LfoTriggerMode.AUTO} onChange={updateStateHandler("TRIG_MODE")}/>AUTO<br/>
+                        <input type="radio" name="trigMode" value={LfoTriggerMode.MAN} checked={state.TRIG_MODE === LfoTriggerMode.MAN} onChange={updateStateHandler("TRIG_MODE")}/>MAN<br/>
                     </td>
                 </tr> 
             </tbody>
